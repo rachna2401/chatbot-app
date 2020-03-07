3c8224365d2d7e4165f27967f1f094342e146252
rachna2401 <rachana.sable@raisoni.net>
2020-03-07 12:12:52 +0530

    initial ui code

---
+++ chatbot-ui/js/main.js
@@ -0,0 +1,36 @@
+function loadHome() {
+    $("#content").load("html/home.html");
+}
+
+function loadBots() {
+    //make $.ajax call 
+    var resp = {
+        "bots": [{"id":"1", "name": "My buddy", "description":"This is your friendly chatbot"},
+        {"id":"2", "name": "My buddy 2", "description":"This is another your friendly chatbot"}]
+    }
+    bots = resp['bots']
+    for(var i=0; i<bots.length;i++) {
+        bot = bots[i];
+        var x = "<div class='xs-4'><div class='card' style='width: 18rem;'><div class='card-body'><h5 class='card-title' id='"+ bot["id"] +  "'>" + bot["name"] + "</h5><p class='card-text'>" + bot["description"] + "</p><a href='#' id='chat' class='btn btn-primary'>Chat</a></div></div></div>"
+        $("#botList").append(x);
+    }
+    
+}
+
+$(document).ready(function() {
+
+    loadHome();
+
+    $("#navHome").click(function() {
+        loadHome();
+    });
+
+    $("#navBots").click(function() {
+        $("#content").load("html/bots.html", loadBots);
+    });
+    
+
+    $(document).on('click', '#chat', function() {
+       alert($(this).text()); 
+    });
+})
